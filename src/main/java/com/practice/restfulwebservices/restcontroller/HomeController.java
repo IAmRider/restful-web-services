820ec68bd36093e2af778953b8975e63e74f9590
vikingsuraj <surajdhembre8@gmail.com>
2021-10-27 08:37:57 +0530

    logger added

--- src/main/java/com/practice/restfulwebservices/restcontroller/HomeController.java
+++ src/main/java/com/practice/restfulwebservices/restcontroller/HomeController.java
@@ -3,6 +3,7 @@ package com.practice.restfulwebservices.restcontroller;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jboss.logging.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	
 	private final int CRITERIA_AGE=27;
-
+	private Logger logger=Logger.getLogger(HomeController.class);
+	
 	@GetMapping("/")
 	public ResponseEntity<String> welcomeToSystem() {
+		logger.debug("root of application endpoint called");
 		return new ResponseEntity<>("Welcome suraj in sprint boot rest api project", HttpStatus.OK);
 	}
 
 	@GetMapping("/employee/{age}")
 	public ResponseEntity<String> getStatusForAge(@PathVariable int age, HttpServletResponse response) {
 
+		logger.debug("getStatusForAge method called for homecontroller");
 		Cookie cookie = new Cookie("age", String.valueOf(age));
 		cookie.setComment("for confirmation that user sent same age");
 		cookie.setMaxAge(200);
